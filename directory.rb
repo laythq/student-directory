e212b902d1bbec856eb467dcc492cea98e06de98
Layth <laythq@gmail.com>
2018-06-06 09:59:13 +0100

    Prints index with names, and only if specific letter

--- directory.rb
+++ directory.rb
@@ -20,12 +20,26 @@ def print_header
   puts "-------------"
 end
 
+def print_names_and_cohort(students)
+  students.each do |x|
+    puts "#{x[:name]} (#{x[:cohort]} cohort)"
+  end
+end
 
-def print_names_and_cohort_2(students)
+def print_index_names_and_cohort(students)
   students.each_with_index {
     |(key, value), y|
     puts "#{y + 1}. #{key[:name]} (#{key[:cohort]} cohort)"}
 end
+
+def print_name_if_first_letter(students)
+    students.each do |x|
+      if x[:name].chars.first == "A"
+        puts x[:name]
+      end
+    end
+end
+
 
 def print_footer(students)
   print "Overall we have #{students.count} great students"
@@ -33,5 +47,5 @@ end
 
 students = input_students
 print_header
-print_names_and_cohort_2(students)
+print_names_and_cohort_3(students)
 print_footer(students)

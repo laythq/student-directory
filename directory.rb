bd2fd88082a95f9dc493b8771ff0ac70d22dd895
Layth <laythq@gmail.com>
2018-06-06 10:47:31 +0100

    Refactoring

--- directory.rb
+++ directory.rb
@@ -1,18 +1,16 @@
+@students = []
+
 def input_students
   puts "Please enter the name of a students"
   puts "To finish, just hit return twice"
 
-  students = []
-
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-
-  students
 end
 
 def print_header
@@ -44,26 +42,36 @@ def print_footer(students)
   print "Overall we have #{students.count} great students"
 end
 
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print_names_and_cohort(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-      end
+def process(selection)
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+end
+
+def show_students
+  print_header
+  print_names_and_cohort(@students)
+  print_footer(@students)
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+
     end
 end
 
